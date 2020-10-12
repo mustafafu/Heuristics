7eea490f82ca714b0012ef371c5fec5fe15977dc
mustafafu <furkanozkoc@gmail.com>
2020-10-12 16:44:46 -0400

    Bash script to take care of files from other directories

--- Week4-AmbulancePickup/meow_run.sh
+++ Week4-AmbulancePickup/meow_run.sh
@@ -2,4 +2,8 @@
 
 input=$1
 
-python3 meow_amb.py --input $input --output meow_out_$input
+output_tmp=`basename $input`
+
+echo > meow_out_$output_tmp
+
+python3 meow_amb.py --input $input --output meow_out_$output_tmp
